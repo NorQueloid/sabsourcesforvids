8c725c976aa64d46dd5fda955ec29ac9713fee99
HugeGAMES <blazeghast001@gmail.com>
2025-10-21 08:55:00 +0800

    Update inv+leavemain.lua

--- inv+leavemain.lua
+++ inv+leavemain.lua
@@ -1,5 +1,5 @@
 local function script1()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/NorQueloid/sabsourcesforvids/refs/heads/main/invis.lua'))()
+    loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/2341c827712daf923191e93377656f67.lua'))()
 end
 local function script2()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/NorQueloid/sab/refs/heads/main/Leavepreventer.lua'))()
